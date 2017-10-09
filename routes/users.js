09c2d50e70459af6707994c3321ad36552d3d120
peterjostiguy <peterjostiguy@gmail.com>
2017-10-09 15:09:22 -0400

    first draft, including solutions

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,61 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var db = require('../db/api')
+var bcrypt = require('bcrypt')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/signin', function(req, res, next){
+  db.signIn()
+  .then(function(agent){
+    //Use bcrypt to log in
+      if (isMatch) {
+        res.send('Welome back, agent ' + agent.agentName)
+      }
+      else {
+        res.render('index', { title: 'gClassified', message: 'Incorrect login. Contents will self destruct' })
+      }
+  })
+})
+
+// router.post('/signin', function(req, res, next){
+//   db.signIn(req.body.agentId, req.body.password)
+//   .then(function(agent){
+//     bcrypt.compare(req.body.password, agent.password, function(err, isMatch){
+//       if (isMatch) {
+//         res.send('Welome back, agent ' + agent.agentName)
+//       }
+//       else {
+//         res.render('index', { title: 'gClassified', message: 'Incorrect login. Contents will self destruct' })
+//       }
+//     })
+//   })
+// })
+
+router.post('/signup', function(req,res,next){
+  //Use bcrypt to Sign Up
+
+    db.signUp()
+    .then(function(agent){
+      if (agent[0] === req.body.password) {
+        res.render('index', { title: 'gClassified', message: 'Password Must Be Hashed. Government Secrets are at Stake!' })
+      }
+      else {
+        res.render('index', { title: 'gClassified', message: 'Sign Up Successful' })
+      }
+    })
+})
+
+// router.post('/signedup', function(req,res,next){
+//   bcrypt.hash(req.body.password, 10, function(err, hash){
+//     db.signUp(req.body.agentName, hash)
+//     .then(function(agent){
+//       if (agent[0] === req.body.password) {
+//         res.render('index', { title: 'gClassified', message: 'Password Must Be Hashed. Government Secrets are at Stake!' })
+//       }
+//       else {
+//         res.render('index', { title: 'gClassified', message: 'Sign Up Successful' })
+//       }
+//     })
+//   })
+// })
 
 module.exports = router;
